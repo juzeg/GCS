5c9da42d2d017062edb9a5f5cd18a69bd512550e
juzeg <juzeg@DESKTOP-9FQCCN1>
2017-09-27 00:12:33 +0200

    Code cleanup

--- GCS/DataDisplay.cs
+++ GCS/DataDisplay.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GCS
@@ -19,17 +12,14 @@ namespace GCS
 
         private void DataDisplay_Load(object sender, EventArgs e)
         {
-
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
         }
 
         private void checkBox5_CheckedChanged(object sender, EventArgs e)
         {
-
         }
     }
 }
--- GCS/Form1.cs
+++ GCS/Form1.cs
@@ -1,44 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Ports;
 using System.Windows.Forms;
 
 namespace GCS
 {
     public partial class GCS : Form
     {
+        public DataDisplay DD;
+        public LoadMission LM;
+        public NewMission NM;
+
+
+
         public GCS()
         {
             InitializeComponent();
         }
-
-      public   NewMission NM;
-      public   DataDisplay DD;
 
         private void dataDisplayToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (DD == null)
             {
                 DD = new DataDisplay();
-                DD.FormClosed += new FormClosedEventHandler(DD_FormClosed);
+                DD.FormClosed += DD_FormClosed;
                 DD.Show();
-
             }
-            else DD.Activate();
-
+            else
+            {
+                DD.Activate();
+            }
         }
 
-        void DD_FormClosed(object sender, FormClosedEventArgs e)
+        private void DD_FormClosed(object sender, FormClosedEventArgs e)
         {
-
-
             DD = null;
-
         }
 
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
@@ -46,44 +41,39 @@ namespace GCS
             if (NM == null)
             {
                 NM = new NewMission();
-                NM.FormClosed += new FormClosedEventHandler(NM_FormClosed);
+                NM.FormClosed += NM_FormClosed;
                 NM.Show();
-
             }
-            else NM.Activate();
-            
+            else
+            {
+                NM.Activate();
+            }
         }
-        void NM_FormClosed(object seender, FormClosedEventArgs e)
+
+        private void NM_FormClosed(object seender, FormClosedEventArgs e)
         {
             NM = null;
-
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-          
         }
 
-        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-
             textBox1.Text += serialPort1.ReadLine();
             textBox1.Text += "\n";
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-
         }
 
         private void SendCommand_Click(object sender, EventArgs e)
         {
             if (serialPort1.IsOpen)
-            {
                 serialPort1.WriteLine(textBox2.Text);
-            }
             else
-            {
                 try
                 {
                     serialPort1.PortName = comboBox1.Text;
@@ -94,9 +84,6 @@ namespace GCS
                 {
                     textBox1.Text += "connection cannont be estabilished \n";
                 }
-                
-               
-            }
             textBox2.Text = null;
         }
     }
